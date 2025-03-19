31bd51c238fa8a327b379c1c01e402293d393bfa
Romain Voivenel <voivenelromain@gmail.com>
2025-03-19 15:34:24 +0100

    debut classe character

---
+++ js/model/character.js
@@ -0,0 +1,60 @@
+export default class Character {
+    constructor(id, nom, description) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.caracteristiques = {};
+        this.items = [];
+    }
+
+    // ID
+    get id() {
+        return this.id;
+    }
+
+    // Nom
+    get nom() {
+        return this.nom;
+    }
+
+    set nom(newNom) {
+        this.nom = newNom
+    }
+
+    // Description
+    get description() {
+        return this.description;
+    }
+
+    set description(newDesc) {
+        this.description = newDesc
+    }
+
+    // Caracteristique
+    get caracteristiques() {
+        return this.caracteristiques;
+    }
+
+    addCarac(nom, baseValue=0) {
+        if (!this.caracteristiques.hasOwnProperty(nom)) {
+            this.caracteristiques[nom] = baseValue;
+        }
+    }
+
+    deleteCarac(nom) {
+        if (this.caracteristiques.hasOwnProperty(nom)) {
+            delete this.caracteristiques[nom]
+        }
+    }
+
+    updateCarac(nom, newValue) {
+        if (this.caracteristiques.hasOwnProperty(nom)) {
+            this.caracteristiques[nom] += newValue
+        }
+    }
+
+    // Item
+    get items() {
+        return this.items;
+    }
+}
