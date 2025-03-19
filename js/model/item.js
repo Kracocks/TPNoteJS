aa162aaadfa2a8cae8ad8e6d915f2bc2f3e5821a
Romain Voivenel <voivenelromain@gmail.com>
2025-03-19 16:14:04 +0100

    debut classe item

---
+++ js/model/item.js
@@ -0,0 +1,8 @@
+export default class Item {
+    constructor(id, nom, description, type) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.type = type;
+    }
+}
