93a22bbeb1a63e6d3403300e1db209f3a7f28d57
Romain Voivenel <voivenelromain@gmail.com>
2025-03-20 08:35:56 +0100

    classe type

---
+++ js/model/type.js
@@ -0,0 +1,8 @@
+export default class Type {
+    static ARME = "Arme";
+    static CASQUE = "Casque";
+    static PLASTRON = "Plastron";
+    static JAMBIERE = "Jambiere";
+    static BOTTE = "Botte";
+    static OBJET = "Objet";
+}
