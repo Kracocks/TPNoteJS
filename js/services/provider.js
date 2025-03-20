412ac30ae7970e7d46fa02d67cb0ecab1e5b7db3
Romain Voivenel <voivenelromain@gmail.com>
2025-03-20 09:28:35 +0100

    Item provider

--- js/services/provider.js
+++ js/services/provider.js
@@ -52,3 +52,57 @@ export default class CharacterProvider {
         }
     }
 }
+
+import { Item } from "../model/character.js"
+
+export class ItemProvider {
+    static fetchItem = async (limit = 10) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}?_limit=${limit}`, options);
+            const json = await response.json();
+            i = json.item.items
+            var items = []
+            i.forEach(jsonItem => {
+                var item = new Item(
+                    jsonItem.id,
+                    jsonItem.nom,
+                    jsonItem.description,
+                );
+                for (const [nom, value] of Object.entries(c.caracteristique)) {
+                    item.addCarac(nom, value)
+                }
+                items.push(item)
+            });
+            return items;
+        } catch (err) {
+            console.log('Error getting characters\n', err);
+        }
+    }
+
+    static getItem = async (id) => {
+
+        
+        const response = await fetch(`${ENDPOINT}/` + id, options);
+        const json = await response.json();
+        i = json.item.items
+        var items = []
+        i.forEach(jsonItem => {
+            var item = new Item(
+                jsonItem.id,
+                jsonItem.nom,
+                jsonItem.description,
+            );
+            for (const [nom, value] of Object.entries(c.caracteristique)) {
+                item.addCarac(nom, value)
+            }
+            items.push(item)
+        });
+        return items;
+    }
+}
