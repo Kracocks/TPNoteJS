8b0606d67ac0f86ce72a8b3614270bb29a04a297
Romain Voivenel <voivenelromain@gmail.com>
2025-03-20 08:19:53 +0100

    getter et setter item

--- js/model/item.js
+++ js/model/item.js
@@ -4,5 +4,61 @@ export default class Item {
         this.nom = nom;
         this.description = description;
         this.type = type;
+        this.caracteristiques = {}
+    }
+
+    // ID
+    get id() {
+        return this.id;
+    }
+
+    // Nom
+    get nom() {
+        return this.nom;
+    }
+
+    set nom(nom) {
+        this.nom = nom;
+    }
+
+    // Description
+    get description() {
+        return this.description;
+    }
+
+    set description(description) {
+        this.description = description;
+    }
+
+    // Type
+    get type(){
+        return this.type;
+    }
+
+    set type(type) {
+        this.type = type;
+    }
+
+    // Caracteristique
+    get caracteristiques() {
+        return this.caracteristiques;
+    }
+
+    addCarac(nom, baseValue=0) {
+        if (!this.caracteristiques.hasOwnProperty(nom)) {
+            this.caracteristiques[nom] = baseValue;
+        }
+    }
+
+    deleteCarac(nom) {
+        if (this.caracteristiques.hasOwnProperty(nom)) {
+            delete this.caracteristiques[nom]
+        }
+    }
+
+    updateCarac(nom, newValue) {
+        if (this.caracteristiques.hasOwnProperty(nom)) {
+            this.caracteristiques[nom] += newValue
+        }
     }
 }
