d9aefe78b14d670c0b9ccc8765d5933e0c858672
Kracocks <voivenelromain@gmail.com>
2025-03-27 00:39:45 +0100

    ajout item provider

--- js/services/provider.js
+++ js/services/provider.js
@@ -33,3 +33,37 @@ export default class CharacterProvider {
         }
     }
 }
+
+export class ItemProvider {
+    static fetchItems = async (limit = 4) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/items?_limit=${limit}`, options);
+            const json = await response.json();
+            return json;
+        } catch (err) {
+            console.log('Error getting items\n', err);
+        }
+    }
+
+    static getItem = async (id) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/items` + id, options);
+            const json = await response.json();
+            return json;
+        } catch (err) {
+            console.log('Error getting item\n', err);
+        }
+    }
+}
