e3f23a9bdc54e0d4f1c905029aef3ef892c01e9e
Kracocks <voivenelromain@gmail.com>
2025-03-27 07:30:29 +0100

    afficher note

--- js/services/itemProvider.js
+++ js/services/itemProvider.js
@@ -1,4 +1,5 @@
 import { ENDPOINT } from "../config.js";
+import Item from "../models/item.js";
 
 export default class ItemProvider {
     static fetchItems = async (limit = 4) => {
@@ -11,7 +12,11 @@ export default class ItemProvider {
         try {
             const response = await fetch(`${ENDPOINT}/items?_limit=${limit}`, options);
             const json = await response.json();
-            return json;
+            let res = [];
+            json.forEach(item => {
+                res.push(new Item(item.id, item.nom, item.description, item.type));
+            });
+            return res;
         } catch (err) {
             console.log('Error getting items\n', err);
         }
@@ -27,7 +32,7 @@ export default class ItemProvider {
         try {
             const response = await fetch(`${ENDPOINT}/items` + id, options);
             const json = await response.json();
-            return json;
+            return new Item(json.id, json.nom, json.description, json.type);
         } catch (err) {
             console.log('Error getting item\n', err);
         }
--- js/views/pages/showItem.js
+++ js/views/pages/showItem.js
@@ -2,24 +2,35 @@ import Utils from '../../services/utils.js';
 import ItemProvider from '../../services/itemProvider.js';
 
 export default class ShowCharacter {
-    static async render(){
+    static async render() {
         let request = Utils.parseRequestURL();
-        let items = await ItemProvider.getItem(request.id);
+        let item = await ItemProvider.getItem(request.id);
+
+        let note = Array.from({ length: 5 }, (_, i) => `
+                            <li>
+                                <input type="radio" id="note${item.id}-${i}" name="note-${item.id}" value="${i}"/>
+                                <label for="note${item.id}-${i}">${i}</label>
+                            </li>
+                        `).join('');
+
+        let caracteristiques = Object.entries(item.caracteristique).map(([key, value]) => `
+            <li>
+                <p>${key} -> ${value}</p>
+            </li>
+        `).join('');
 
         return `
             <section class="character">
-                <h2>${items.nom}</h2>
-                <p>${items.description}</p>
-                <p>${items.type}</p>
-                <p>
+                <h2>${item.nom}</h2>
+                <p>${item.description}</p>
+                <p>${item.type}</p>
                 <ul>
-                    ${items.caracteristique.map((carac) => `
-                        <li>
-                            <p>${carac} -> ${items.caracteristique[carac]}</p>
-                        </li>`
-                    )}
+                    ${caracteristiques}
                 </ul>
-                </p><img loading="lazy" src="../../../data/c${items.id}.jpg" alt="${items.nom}">
+                <ul>
+                    ${note}
+                </ul>
+                <img loading="lazy" src="../../../data/c${item.id}.jpg" alt="${item.nom}">
             </section>
         `;
     }
